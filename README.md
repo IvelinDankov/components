bf0d8a55b5481995267925ceb7721d8b80e8a357
Velin Dankov <velin80@web.de>
2025-06-23 09:37:19 +0300

    adding list component.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ The Idea Behind Components.
 A Component controls part of the screen.
 You define app logic into component.
 Each component has its own html and css..
+
+## To make your angular app `ng new <project name>`.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { ListComponent } from "./list/list.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  imports: [RouterOutlet, ListComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'components';
+  title = "components";
 }
