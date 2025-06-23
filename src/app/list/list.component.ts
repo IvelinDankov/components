70611153ea3938c26e50c60a96677c7380fe6646
Velin Dankov <velin80@web.de>
2025-06-23 10:30:24 +0300

    For Block

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-list',
+  selector: "app-list",
   imports: [],
-  templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  templateUrl: "./list.component.html",
+  styleUrl: "./list.component.scss",
 })
 export class ListComponent {
+  myPropt = `Hello world!`;
 
+  users = [
+    {
+      firstName: "Ivan",
+      lastName: "Ivanov",
+    },
+    {
+      firstName: "Peter",
+      lastName: "Petrov",
+    },
+    {
+      firstName: "Stamat",
+      lastName: "Stamatov",
+    },
+    {
+      firstName: "Pescho",
+      lastName: "Peschev",
+    },
+  ];
+
+  constructor() {
+    // setTimeout(() => {
+    //   this.myPropt = `Hello after 5 seconds!`;
+    // }, 5000);
+  }
 }
